1beb6a6fd0185daabb3748e2f869a0afb59c051d
timam <me@timam.io>
2018-11-03 01:41:06 +0600

    taking directory based backup

--- backup.py
+++ backup.py
@@ -1,9 +1,11 @@
 import boto3, subprocess
-from time import gmtime, strftime
+from time import strftime
+
 
 client = boto3.client('s3')
-gimme_time = strftime("%Y-%m-%d-%H-%M", gmtime())
-app_name = ''
+date_today = strftime("%Y-%m-%d")
+
+app_name = 'swin'
 
 source_bucket = 'saif-cloudfront-test'
 backup_bucket = 'saif-timam-terraform'
@@ -11,21 +13,19 @@ backup_bucket = 'saif-timam-terraform'
 
 def gimme_source():
     if not app_name:
-        return 's3://' + source_bucket
+        return 's3://' + source_bucket + '/'
     else:
-        return 's3://' + source_bucket + '/' + app_name
+        return 's3://' + source_bucket + '/' + app_name + '/'
 
 
 def gimme_destination():
     if not app_name:
-        return 's3://' + backup_bucket + '/' + source_bucket + '-' + gimme_time + '/'
+        return 's3://' + backup_bucket + '/' + source_bucket + '-' + date_today + '/'
     else:
-        return 's3://' + backup_bucket + '/' + app_name + '/' + gimme_time + '/'
+        return 's3://' + backup_bucket + '/' + app_name + '/' + date_today+ '/'
 
 
 source = gimme_source()
 destination = gimme_destination()
 
-subprocess.check_output(['aws', 's3', 'sync', '--delete', source, destination])
-
-
+subprocess.check_output(['aws', 's3', 'sync', source, destination])
