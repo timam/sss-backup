86e2abda35035a47e63e1fa0865f3ccf74060ffc
timam <me@timam.io>
2018-11-03 05:08:02 +0600

    older then one week backups are deleting

--- backup.py
+++ backup.py
@@ -35,6 +35,27 @@ def gimme_last_seven_days():
     return days_of_last_week
 
 
+def gimme_list_of_backups():
+    backup_list = []
+
+    app_path = 's3://' + backup_bucket + '/' + app_name + '/'
+    all_backups = subprocess.check_output(['aws', 's3', 'ls', app_path]).decode('utf-8')
+
+    raw_backup_list = all_backups.split('/')
+
+    for item in raw_backup_list:
+        date = item.replace('PRE ', '').lstrip()
+        backup_list.append(date)
+
+    return backup_list[:-1]
+
+
+def gimme_cleanup_list(last_seven_days, list_of_backups):
+    cleanup = list(set(list_of_backups) - set(last_seven_days))
+
+    return cleanup
+
+
 # Gathering Source and Destination Information
 source = gimme_source()
 destination = gimme_destination()
@@ -44,3 +65,10 @@ subprocess.check_output(['aws', 's3', 'sync', source, destination])
 
 # Gathering Backup Cleanup Information
 last_seven_days = gimme_last_seven_days()
+list_of_backups = gimme_list_of_backups()
+cleanup_list = gimme_cleanup_list(last_seven_days, list_of_backups)
+
+for item in cleanup_list:
+    path = 's3://' + backup_bucket + '/' + app_name + '/' + item + '/'
+    subprocess.check_output(['aws', 's3', 'rm', '--recursive', path])
+
