a7f26cca955471af96fd169e14e78d2a1a92a771
timam <me@timam.io>
2018-11-02 13:39:12 +0600

    adding appname dynamicly

--- backup.py
+++ backup.py
@@ -3,21 +3,29 @@ from time import gmtime, strftime
 
 client = boto3.client('s3')
 gimme_time = strftime("%Y-%m-%d-%H-%M", gmtime())
+app_name = ''
 
 source_bucket = 'saif-cloudfront-test'
 backup_bucket = 'saif-timam-terraform'
 
-create_dir = client.put_object(
-        Bucket=backup_bucket,
-        Key=gimme_time+'/',
-        )
-source = 's3://'+source_bucket
-destination = 's3://'+backup_bucket+'/'+gimme_time+'/'
 
+def gimme_source():
+    if not app_name:
+        return 's3://' + source_bucket
+    else:
+        return 's3://' + source_bucket + '/' + app_name
+
+
+def gimme_destination():
+    if not app_name:
+        return 's3://' + backup_bucket + '/' + source_bucket + '-' + gimme_time + '/'
+    else:
+        return 's3://' + backup_bucket + '/' + app_name + '/' + gimme_time + '/'
+
+
+source = gimme_source()
+destination = gimme_destination()
 
 subprocess.check_output(['aws', 's3', 'sync', '--delete', source, destination])
 
-# seperare folder
-# 7 days delete
-# management server
-# how long - lifecycle
+
