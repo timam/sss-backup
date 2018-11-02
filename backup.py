589484161c4c9d222f6dc2036c235249e17dda2b
timam <me@timam.io>
2018-11-03 02:57:05 +0600

    generating last seven days list

--- backup.py
+++ backup.py
@@ -1,9 +1,9 @@
 import boto3, subprocess
-from time import strftime
+from datetime import datetime, timedelta
 
 
 client = boto3.client('s3')
-date_today = strftime("%Y-%m-%d")
+date_today = datetime.now().date()
 
 app_name = 'swin'
 
@@ -20,12 +20,27 @@ def gimme_source():
 
 def gimme_destination():
     if not app_name:
-        return 's3://' + backup_bucket + '/' + source_bucket + '-' + date_today + '/'
+        return 's3://' + backup_bucket + '/' + source_bucket + '-' + str(date_today) + '/'
     else:
-        return 's3://' + backup_bucket + '/' + app_name + '/' + date_today+ '/'
+        return 's3://' + backup_bucket + '/' + app_name + '/' + str(date_today) + '/'
 
 
+def gimme_last_seven_days():
+    days_of_last_week = []
+
+    for day in range(0, 7):
+        day = date_today - timedelta(days=day)
+        days_of_last_week.append(str(day))
+
+    return days_of_last_week
+
+
+# Gathering Source and Destination Information
 source = gimme_source()
 destination = gimme_destination()
 
+# BackingUp
 subprocess.check_output(['aws', 's3', 'sync', source, destination])
+
+# Gathering Backup Cleanup Information
+last_seven_days = gimme_last_seven_days()
