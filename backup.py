be1697931b359c2512243e27ee703ac65911fbff
timam <me@timam.io>
2018-11-03 17:45:18 +0600

    dynamic dirname functionality, removed cleanup

--- backup.py
+++ backup.py
@@ -1,99 +1,60 @@
 import boto3, subprocess
-from datetime import datetime, timedelta
+from datetime import datetime
 
-
-client = boto3.client('s3')
-date_today = datetime.now().date()
-
-app_name = 'swin'
 
 source_bucket = 'saif-cloudfront-test'
 backup_bucket = 'saif-timam-terraform'
 
 
-def gimme_source():
-    if not app_name:
-        return 's3://' + source_bucket + '/'
-    else:
-        return 's3://' + source_bucket + '/' + app_name + '/'
+def gimme_list_of_apps():
+    app_list = []
+
+    bucket = 's3://' + source_bucket + '/'
+    all_apps = subprocess.check_output(['aws', 's3', 'ls', bucket]).decode('utf-8')
+
+    raw_app_list = all_apps.split('/')
+
+    for item in raw_app_list:
+        date = item.replace('PRE ', '').lstrip()
+        app_list.append(date)
+
+    return app_list[:-1]
+
+
+def gimme_source(app_name):
+    return 's3://' + source_bucket + '/' + app_name + '/'
+
+
+def gimme_destination(app_name):
+    return 's3://' + backup_bucket + '/' + app_name + '/' + str(date_today) + '/'
+
+
+def display_generic_message():
+    print("Today is : " + str(date_today))
+    print(str(len(list_of_apps)) + " application form bucket " + source_bucket + " marked for backup")
 
 
 def display_backing_up_message():
-    print('='*50 +"\nToday is : " + str(date_today))
-    print("Taking backup of : " + app_name)
+    print('-' * 70 + "\nBacking Up : " + source_bucket + '/' + app_name)
 
 
-def display_backup_success_message():
-    print('='*50 +"\nBackup for " + app_name + " is completed")
-    print("Backup is available on : " + destination)
+def display_backup_compilation_message():
+    print("Backup Completed : " + app_name)
+    print("Backup Available on : " + destination + "\n" + '-' * 70)
 
 
-def gimme_destination():
-    if not app_name:
-        return 's3://' + backup_bucket + '/' + source_bucket + '-' + str(date_today) + '/'
-    else:
-        return 's3://' + backup_bucket + '/' + app_name + '/' + str(date_today) + '/'
+list_of_apps = gimme_list_of_apps()
+date_today = datetime.now().date()
 
+display_generic_message()
 
-def gimme_last_seven_days():
-    days_of_last_week = []
+for app_name in list_of_apps:
 
-    for day in range(0, 7):
-        day = date_today - timedelta(days=day)
-        days_of_last_week.append(str(day))
+    source = gimme_source(app_name)
+    destination = gimme_destination(app_name)
 
-    return days_of_last_week
+    display_backing_up_message()
 
+    subprocess.check_output(['aws', 's3', 'sync', source, destination])
 
-def gimme_list_of_backups():
-    backup_list = []
-
-    app_path = 's3://' + backup_bucket + '/' + app_name + '/'
-    all_backups = subprocess.check_output(['aws', 's3', 'ls', app_path]).decode('utf-8')
-
-    raw_backup_list = all_backups.split('/')
-
-    for item in raw_backup_list:
-        date = item.replace('PRE ', '').lstrip()
-        backup_list.append(date)
-
-    return backup_list[:-1]
-
-
-def gimme_cleanup_list(last_seven_days, list_of_backups):
-    cleanup = list(set(list_of_backups) - set(last_seven_days))
-
-    return cleanup
-
-
-def display_cleanup_message(cleanup_list):
-    if len(cleanup_list) == 0 :
-        print('='*50 + "\nIdentified " + str(len(cleanup_list)) + " backup for cleanup")
-        print("Nothing to do...")
-    else:
-        print('='*50 + "\nIdentified " + str(len(cleanup_list)) + " backup for cleanup")
-        print("Cleaning Up : " + str(cleanup_list))
-
-
-
-# Gathering Source and Destination Information
-source = gimme_source()
-destination = gimme_destination()
-
-# BackingUp
-display_backing_up_message()
-subprocess.check_output(['aws', 's3', 'sync', source, destination])
-display_backup_success_message()
-
-# Gathering Backup Cleanup Information
-last_seven_days = gimme_last_seven_days()
-list_of_backups = gimme_list_of_backups()
-cleanup_list = gimme_cleanup_list(last_seven_days, list_of_backups)
-
-display_cleanup_message(cleanup_list)
-
-for item in cleanup_list:
-    path = 's3://' + backup_bucket + '/' + app_name + '/' + item + '/'
-    subprocess.check_output(['aws', 's3', 'rm', '--recursive', path])
-    print("Cleanup Done : " + item)
-
+    display_backup_compilation_message()
